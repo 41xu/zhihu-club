c65dac72f017e71085d30e0c178b95807ef2ec0d
41xu <xu_sy11111@mail.dlut.edu.cn>
2020-02-09 13:39:33 +0800

    updata doc, save data to csv

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # zhihu-club
-spider about zhihu club.
+一个爬[知乎圈子](https://www.zhihu.com/club/explore)的小爬虫
+
+## 安装
+
+```
+pip3 install -r requirements.txt
+```
+
+## 配置
+
+默认使用我的服务器上的mysql数据库，如果你想更改成你的数据库，请在`/zhihu/zhihu/setting.py`中进行如下修改
+
+```
+MYSQL_HOST=‘your_host’
+MYSQL_DATABASE='your_database'
+MYSQL_PORT=3306 # 默认端口号
+MYSQL_USER='your_user'
+MYSQL_PASSWORD='your_password' 
+```
+
+## 运行
+
+下载项目解压并安装好依赖后
+
+```
+cd zhihu/
+scrapy crawl zhihu-club 
+```
+
+爬虫运行结束，将爬到的数据从我的mysql数据库中导出为csv文件
+
+```
+cd .. # 进入zhihu-club这个主文件夹中 
+python3 savedata.py # 若data中已存在目标文件.csv则需先把那个文件删除或者在savedata里修改要写入的文件名
+```
+
---
+++ savedata.py
@@ -0,0 +1,58 @@
+import pymysql
+import csv
+
+
+def get_category_info():
+    connect = pymysql.connect(
+        host="114.215.184.150",
+        port=3306,
+        user='root',
+        password='990105Xsy!',
+        db='zhihu',
+        charset='utf8mb4'
+    )
+    cursor = connect.cursor()
+    sql = 'select * from category'
+    cursor.execute(sql)
+    category = cursor.fetchall()
+    connect.close()
+    return category
+
+
+def get_club_info():
+    connect = pymysql.connect(
+        host="114.215.184.150",
+        port=3306,
+        user='root',
+        password='990105Xsy!',
+        db='zhihu',
+        charset='utf8mb4'
+    )
+    cursor = connect.cursor()
+    sql = 'select * from club'
+    cursor.execute(sql)
+    club = cursor.fetchall()
+    connect.close()
+    return club
+
+
+def category_to_csv():
+    data=get_category_info()
+    filename='data/category.csv'
+    with open(filename,'x') as f:
+        write=csv.writer(f,dialect='excel')
+        for item in data:
+            write.writerow(item)
+
+
+def club_to_csv():
+    data=get_club_info()
+    filename='data/club.csv'
+    with open(filename,'x') as f:
+        write=csv.writer(f,dialect='excel')
+        for item in data:
+            write.writerow(item)
+
+if __name__=='__main__':
+    category_to_csv()
+    club_to_csv()
